3473a1459f557dbb38bc403014b3a69ee8ee5025
robz <codinwithrobz@gmail.com>
2022-10-10 17:02:26 -0600

    implement easy ai

--- main.js
+++ main.js
@@ -90,7 +90,14 @@ const playMove = (box, data) => {
 
   //change current player
   //change the dom, and change data.currentplayer
-  changePlayer(data);
+  if (data.choice === 0) {
+    changePlayer(data);
+  } else if (data.choice === 1) {
+    //easy ai
+    easyAiMove(data);
+    data.currentPlayer = "X";
+    //change back to player1
+  }
 };
 
 const endConditions = (data) => {
@@ -138,4 +145,27 @@ const changePlayer = (data) => {
   let displayTurnText =
     data.currentPlayer === "X" ? data.player1Name : data.player2Name;
   adjustDom("displayTurn", `${displayTurnText}'s turn`);
+};
+
+const easyAiMove = (data) => {
+  changePlayer(data);
+
+  setTimeout(() => {
+    let availableSpaces = data.board.filter(
+      (space) => space !== "X" && space !== "O"
+    );
+    let move =
+      availableSpaces[Math.floor(Math.random() * availableSpaces.length)];
+    console.log(move);
+    data.board[move] = data.player2;
+    let box = document.getElementById(`${move}`);
+    box.textContent = data.player2;
+    box.classList.add("player2");
+  }, 200);
+
+  if (endConditions(data)) {
+    return;
+  }
+
+  changePlayer(data);
 };
